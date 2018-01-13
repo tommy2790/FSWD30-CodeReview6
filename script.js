ec7a2a01f4b7d64dc732e9a8354276e3fe2e3885
Thomas Foller <thomasfoller@gmail.com>
2018-01-13 17:05:37 +0100

    asdf
    asdf

---
+++ script.js
@@ -0,0 +1,87 @@
+var albums = [
+            {
+            title: 'Never Mind The Bollocks',
+            author: 'Sex Pistols',
+            genre: 'Rock',
+            publisher: 'United Artists',    
+            rating: 5,
+            },
+            
+            {
+            title: 'Sgt. Peppers Lonely Hearts Club Band',
+            author: 'The Beatles',
+            genre: 'Rock',
+            publisher: 'Prestige Worldwide',    
+            rating: 4
+            },
+
+            {
+            title: 'Sticky Fingers',
+            author: 'Rolling Stones',
+            genre: 'Rock',
+            publisher: 'United Artists',    
+           	rating: 5
+            },
+
+            {
+            title: 'Hotel California',
+            author: 'The Eagles',
+            genre: 'Rock',
+            publisher: 'Sony Music',   
+            rating: 4
+            },
+
+            {
+            title: 'Abraxas',
+            author: 'Santana',
+            genre: 'Rock',
+            publisher: 'Syco',   
+            rating: 5
+        	},
+
+        	{
+            title: 'Born To Run',
+            author: 'Bruce Springsteen',
+            genre: 'Rock',
+            publisher: 'WMG',   
+            rating: 5
+        	},
+
+
+		];
+
+		$("#title0").append( albums[0].title);
+		$("#author0").append( albums[0].author);
+		$("#genre0").append( albums[0].genre);
+		$("#publisher0").append( albums[0].publisher);
+		$("#rating0").append (albums[0].rating);
+
+		$("#title1").append( albums[1].title);
+		$("#author1").append( albums[1].author);
+		$("#genre1").append( albums[1].genre);
+		$("#publisher1").append( albums[1].publisher);
+		$("#rating1").append (albums[1].rating);
+
+		$("#title2").append( albums[2].title);
+		$("#author2").append( albums[2].author);
+		$("#genre2").append( albums[2].genre);
+		$("#publisher2").append( albums[2].publisher);
+		$("#rating2").append (albums[2].rating);
+
+		$("#title3").append( albums[3].title);
+		$("#author3").append( albums[3].author);
+		$("#genre3").append( albums[3].genre);
+		$("#publisher3").append( albums[3].publisher);
+		$("#rating3").append (albums[3].rating);
+
+		$("#title4").append( albums[4].title);
+		$("#author4").append( albums[4].author);
+		$("#genre4").append( albums[4].genre);
+		$("#publisher4").append( albums[4].publisher);
+		$("#rating4").append (albums[4].rating);
+
+		$("#title5").append( albums[5].title);
+		$("#author5").append( albums[5].author);
+		$("#genre5").append( albums[5].genre);
+		$("#publisher5").append( albums[5].publisher);
+		$("#rating5").append (albums[5].rating);
